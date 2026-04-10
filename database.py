805b58b729d3d27b9adc0f41260083477c78e54e
Somiya-manikandan <153815946+Somiya-manikandan@users.noreply.github.com>
2026-04-10 16:07:44 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,29 +3,30 @@ import sqlite3
 conn = sqlite3.connect("database.db")
 c = conn.cursor()
 
+# users
 c.execute("""
 CREATE TABLE IF NOT EXISTS users(
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 name TEXT,
 email TEXT,
-password TEXT
-)
+password TEXT)
 """)
 
+# packages
 c.execute("""
 CREATE TABLE IF NOT EXISTS packages(
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 place TEXT,
-price TEXT
-)
+price TEXT)
 """)
 
+# bookings
 c.execute("""
 CREATE TABLE IF NOT EXISTS bookings(
 id INTEGER PRIMARY KEY AUTOINCREMENT,
-user_id INTEGER,
-package_id INTEGER
-)
+name TEXT,
+place TEXT,
+payment TEXT)
 """)
 
 conn.commit()
